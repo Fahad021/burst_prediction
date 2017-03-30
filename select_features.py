ea5dfb6052166ca66e803acaeb8bd638776a95bc
RianaChen <rdchenrui@gmail.com>
2017-03-30 18:22:35 +0800

    fix

--- select_features.py
+++ select_features.py
@@ -14,6 +14,7 @@ import time
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.externals import joblib
+from sklearn.feature_selection import SelectFromModel
 
 from scipy.signal import argrelextrema
 
