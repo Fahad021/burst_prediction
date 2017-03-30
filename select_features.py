2c92d0fd42f5855cb70dc0370a5a7fb2d3c5b34d
RianaChen <rdchenrui@gmail.com>
2017-03-30 17:51:22 +0800

    update feature selecting

--- select_features.py
+++ select_features.py
@@ -14,14 +14,13 @@ import time
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.externals import joblib
-from sklearn.feature_selection import SelectFromModel
 
 from scipy.signal import argrelextrema
 
 from lstm import *
 from classifier import *
-from burst_refinement import *
-from prepare_v2 import *
+from burst_refinement_v2 import *
+from prepare_v3 import *
 
 
 def get_dates(filename):
@@ -41,25 +40,24 @@ if __name__ == "__main__":
     id_ = 2
     seq_len = 30
     epochs  = 100
-
-
+    
     # load data
     print "load data"
-    bursts_info, burst_series, non_burst_series = get_category_bursts(sys.argv[1], 
+    bursts_info, burst_series, non_burst_series, features, history = get_category_bursts(sys.argv[1], 
                                                    sys.argv[3], 
                                                    sys.argv[4], 
                                                    sys.argv[2], 
                                                    id_, 
                                                    seq_len)
     # use 1/3 as test set
-    B_train, S_train, N_train, B_test, S_test, N_test = train_test_split(
-             bursts_info, burst_series, non_burst_series)
+    B_train, S_train, N_train, B_test, S_test, N_test, F_train, F_test, H_train, H_test = train_test_split(
+             bursts_info, burst_series, non_burst_series, features, history)
     print "train/test size: ", len(S_train), len(S_test)
 
-    t_data_c = get_samples_for_classfier_v2(S_train, N_train, seq_len) # [pos_samples, neg_samples]
+    t_data_c = get_samples_for_classfier(S_train, N_train, F_train, seq_len) # [pos_samples, neg_samples]
 
     # build clf model
-    c_X, c_y = prepare_svm_input(t_data_c[0], t_data_c[1])
+    c_X, c_y = prepare_svm_input_v2(t_data_c[0], t_data_c[1], t_data_c[2], t_data_c[3])
     clf = svm.LinearSVC(C=0.01, penalty="l1", dual=False).fit(c_X, c_y)
     model = SelectFromModel(clf, prefit=True)
     features = model.get_support()
